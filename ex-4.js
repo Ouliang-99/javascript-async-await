ff7b35f73112d31f94b9b9141218e1db6045d7ce
OLEANG <wannachok.job@gmail.com>
2024-10-15 12:55:16 +0700

    Oleang-answer-for-javascript-async-await

--- ex-4.js
+++ ex-4.js
@@ -13,3 +13,14 @@ let getJohnProfile = () => {
   });
 };
 // Start coding here
+
+async function showJohnProfile() {
+  try {
+    const profile = await getJohnProfile();
+    console.log(profile);
+  } catch (error) {
+    console.log(error);
+  }  
+};
+
+showJohnProfile();
